cb791bc993cd7aa98949783983299e0481e97a7c
Matheus Diniz <matheusjp201fjv@gmail.com>
2021-06-10 00:20:15 -0300

    alteração

--- forca.py
+++ forca.py
@@ -1,5 +1,28 @@
 def jogos():
     print("Jogo da forca ! ")
+
+    palavra_escondida = "Astronauta"
+    enforcou = False
+    acertou = False
+
+#enquanto(True e não True)
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra que você deseja escolher? ")
+        chute = chute.strip()
+
+        index = 0
+        
+        for letra in palavra_escondida:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+            
+        
+        
+        print("Vamos continuar jogando")
+
+
     print("Fim de Jogo ")
 
 
